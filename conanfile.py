a07daeffbca143efb311a5f17eef263839c223ee
Daniel Bengtsson <daniel@bengtssons.info>
2017-06-21 20:29:04 +0200

    Use portable installer

--- conanfile.py
+++ conanfile.py
@@ -5,9 +5,7 @@ import os
 
 class PerlinstallerConan(ConanFile):
     """
-    For now this installs for all users with all default settings.
-    A possible improvement would be to use the portable install packages
-    to make a local install instead.
+    Download and unzips portable strawberry perl
     """
     name = "Perl_installer"
     version = "1.0"
@@ -18,31 +16,15 @@ class PerlinstallerConan(ConanFile):
     options = {"version": ["5.26.0.1"]}
     default_options = "version=5.26.0.1"
 
-    def isInstalled(self):
-        """Check if perl is already installed"""
-        try:
-            reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
-            k = OpenKey(reg, r'SOFTWARE\Classes\Perl_program_file\shell\Execute Perl Program\command')
-            self.output.info("Perl already installed: true")
-            return True
-        except WindowsError:
-            self.output.info("Perl already installed: false")
-        return False
-        
     def build(self):
-        if self.isInstalled():
-            return
-        #url = "http://strawberryperl.com/download/{0}/strawberry-perl-{0}-64bit.msi".format(self.options.version)
-        url = "http://bengtssons.info/files/strawberry-perl-5.26.0.1-64bit.msi"
+        url = "http://strawberryperl.com/download/{0}/strawberry-perl-{0}-64bit-portable.zip".format(self.options.version)
         dest = "perl.msi"
         self.output.info("Will download " + url)
         tools.download(url, dest)
-        self.run("msiexec /i {} /qb".format(os.path.abspath('perl.msi')))
+        tools.unzip(dest, "unzipped")
 
     def package(self):
-        """We currently install globally so have nothing to package"""
-        pass
+        self.copy("*", dst="", src="unzipped")
 
     def package_info(self):
-        # FIXME: Let path be an option
-        self.env_info.path.append(r'C:\Strawberry\perl\bin')
+        self.env_info.path.append(os.path.join(self.package_folder, "perl", "bin"))
